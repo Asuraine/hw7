0442cb7a28b2dde4076fdda292421b57caf941d9
Elena <elena.lichman@mail.ru>
2023-10-30 07:52:40 +0300

    hw finish version2

--- src/main/java/ru/sbgeu/lichman/basic/MainApp.java
+++ src/main/java/ru/sbgeu/lichman/basic/MainApp.java
@@ -12,7 +12,7 @@ public class MainApp {
         SIZE = 6;
         randomArray(array);
         System.out.println(sumPositiveElement(randomArray(array)));
-        stars(SIZE);
+        stars();
         dioganal(array);
         dioganalSer(array);
         System.out.println(findMax(array));
@@ -44,7 +44,7 @@ public class MainApp {
         return array;
     }
 
-    public static void stars(int size) {
+    public static void stars() {
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
 
